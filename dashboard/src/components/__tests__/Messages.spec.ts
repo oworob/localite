ab4d1fbf25edf9c6407205f47289caf72596018e
oworobjew <oworobjew@gmail.com>
2025-08-23 18:47:56 +0200

    fix lint

--- dashboard/src/components/__tests__/Messages.spec.ts
+++ dashboard/src/components/__tests__/Messages.spec.ts
@@ -1,5 +1,5 @@
 import { createTestingPinia } from '@pinia/testing'
-import { flushPromises, mount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { messages } from '@/assets/testing/test-messages'
 import LiveService from '@/services/LiveService'
